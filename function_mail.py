34fda316525779bc1155fc1fbd9840ab3ae3cdaf
mat177 <matbak177@gmail.com>
2023-01-16 22:43:02 +0100

    mail

--- function_mail.py
+++ function_mail.py
@@ -6,28 +6,20 @@ from email.mime.multipart import MIMEMultipart
 import pandas as pd
 from email.mime.application import MIMEApplication
 from pretty_html_table import build_table
-# root password
 import mysql.connector
 import pymysql
 import os
 
-# os.environ["nazwa"] = "haslo"
+
 password = os.environ["mysql_password"]
-passoword_mail=os.environ["mail_password"]
+password_mail=os.environ["mail_password"]
 
 mydb = mysql.connector.connect(
-    user='mateusz'
-    , password=password  # oz7
+      user='mateusz'
+    , password=password
     , host='localhost'
     , database='python'
 )
-
-# mydb = pymysql.connect(
-#     user='mateusz'
-#     ,password='oliverzamek7'
-#     ,host='localhost'
-#     ,database='python'
-# )
 
 # jeden sposob pobierania danych z mysql
 receiver_query = mydb.cursor()
@@ -77,7 +69,7 @@ def send_mail():
         message['From'] = 'Analyst - Mateusz'
         message['To'] = i[0]
         message.attach(MIMEText(body, 'html'))
-        server.login('matbak177@gmail.com', passoword_mail)
+        server.login('matbak177@gmail.com', password_mail)
 
         if liczba > 3:
             attachment = MIMEApplication(df.to_csv())
@@ -90,3 +82,5 @@ def send_mail():
         except smtplib.SMTPException as e:
             print(str(e))
         server.quit()
+
+send_mail()
